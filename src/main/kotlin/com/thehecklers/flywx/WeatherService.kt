6b36db6b53609947a2f2c3a1fecc03fb7e3ab37d
Mark Heckler <mark.heckler@gmail.com>
2021-03-15 17:55:53 -0500

    Added code to address errors in upstream request

--- src/main/kotlin/com/thehecklers/flywx/WeatherService.kt
+++ src/main/kotlin/com/thehecklers/flywx/WeatherService.kt
@@ -3,21 +3,25 @@ package com.thehecklers.flywx
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.core.publisher.Mono
 
 @Service
 class WeatherService {
-    val tokenParam = "?token=<insert_token_here>"
+    val tokenParam = "?token=<insert_token_here"
 
     val client = WebClient.create("https://avwx.rest/api/metar/")
 
-fun getMETAR(loc: String?) = client.get()
-    .uri(
-        if (loc.isNullOrEmpty()) {
-            "KALN$tokenParam"
-        } else {
-            "$loc$tokenParam"
-        }
-    )
-    .retrieve()
-    .bodyToMono<METAR>()
+    fun getMETAR(loc: String?) = client.get()
+        .uri(
+            if (loc.isNullOrEmpty()) {
+                "KALN$tokenParam"
+            } else {
+                "$loc$tokenParam"
+            }
+        )
+        .retrieve()
+        .bodyToMono<METAR>()
+        .onErrorReturn(
+            METAR("???", "Error retrieving METAR for location $loc")
+        )
 }
